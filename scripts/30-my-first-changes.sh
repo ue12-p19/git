4853d5c3782ea4d5c750449417f8c268cb80bf48
parmentelat <thierry.parmentelat@inria.fr>
2019-05-10 12:07:55 +0200

    checkpoint: 20 + 20 + 30 + début de 40

--- scripts/30-my-first-changes.sh
+++ scripts/30-my-first-changes.sh
@@ -105,74 +105,11 @@ show-diffs
 
 git reset --hard HEAD
 ../scripts/populate.sh do-both-kinds-of-changes
-show-diffs
 
+# si on fait les deux 
+# c'est comme reset --hard
 
 git reset -- README.md
 git checkout -- README.md
 
 show-diffs
-
-# les fichiers dans l'index,
-# i.e. dans le prochain commit 
-# si on le faisait maintenant
-git ls-files
-
-# pareil 
-# mais pour voir seulement
-# seulement ceux qui sont modifiés
-
-# alias: git ls-files -m
-git ls-files --modified
-
-# imaginons que je détruis un fichier
-rm LICENSE
-
-git ls-files
-
-touch NEW
-
-git ls-files
-
-# je suis actuellement 
-# sur la branche `devel`,
-# un commit qui a 2 fichiers
-
-git ls-tree --abbrev devel
-
-# imaginons que je veuille 
-# revenir à la branche `master`
-
-git ls-tree --abbrev master
-
-# mais que j'aie un fichier `file1` qui traine:
-touch file1
-
-# alors `git` va refuser de revenir à la branche `master`
-git checkout master
-
-# pour revenir à un état propre
-rm file1
-
-# devrait afficher:
-# 
-# On branch devel
-# nothing to commit, working tree clean
-
-git status
-
-# je modifie README.md
-
-echo 'on modifie le readme' >> README.md
-
-git diff README.md
-
-git checkout master
-
-git diff
-
-git status
-
-show-repo
-
-git log
