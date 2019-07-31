38cd7dfae4315cd24463780c036bc294af46d7f8
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2019-07-31 14:28:07 +0200

    moving to readthedocs

--- scripts/1-03-notebooks-bash.sh
+++ scripts/1-03-notebooks-bash.sh
@@ -12,10 +12,11 @@ pwd
 
 # ces deux réglages pour git sont nécessaires
 # 
-# ça se lit comme ceci: si user.name est indéfini, alors on lui donne la chaine "Jeanne Durand"
+# ça se lit comme ceci: si user.name est indéfini, 
+# alors on lui donne la chaine "Jeanne Durand"
 
 git config user.name  || git config --global user.name "Jeanne Durand"
-git config user.email || git config --global user.email "jeanne.durand@example.org"
+git config user.email || git config --global user.email "jeanne.durand@foo.org"
 
 # vérifions le réglage en vigueur
 git config user.name
--- scripts/2-03-my-first-branch.sh
+++ scripts/2-03-my-first-branch.sh
@@ -12,7 +12,7 @@ reset=""
 if [ -n "$reset" ]; then 
     cd $TOP
     bash $SCRIPTS/2-01-my-first-repo.sh 
-    bash $SCRIPTS/2-01-my-first-changes.sh 
+    bash $SCRIPTS/2-02-my-first-changes.sh 
 fi >& /dev/null
 
 # si nécessaire, on se place dans le repo git
